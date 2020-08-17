3067f17721bda71237d96445d8295770c56f95e5
polarbear08 <polaris10377@gmail.com>
2020-08-17 09:57:37 +0000

    [add] added vue card stack

---
+++ pages/playground.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>SimpleCardStack</h2>
+    <simple-card-stack />
+  </div>
+</template>
+
+<script>
+import SimpleCardStack from '@/components/atoms/SimpleCardStack'
+export default {
+  name: 'PlaygroundPage',
+  components: {
+    SimpleCardStack,
+  },
+}
+</script>
---
+++ plugins/plugin.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import VueCardStack from 'vue-card-stack'
+Vue.use(VueCardStack)
